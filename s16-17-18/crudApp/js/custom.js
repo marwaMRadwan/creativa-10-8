6481f2193c7da21943f2c334a04f3b4336ee1c0c
marwaMRadwan <marwaradwan872@hotmail.com>
2025-09-23 20:31:48 +0300

    last

--- s16-17-18/crudApp/js/custom.js
+++ s16-17-18/crudApp/js/custom.js
@@ -72,10 +72,9 @@ const createMyElement = (parent, newEle, classes, txt, href ) =>{
     if(txt) ele.innerText = txt
     return ele
 }
-const dataWrap = document.querySelector("#data-wrap")
-if(dataWrap){
-    const allUsers = readFromLocalStorage()
-    allUsers.forEach((user,i)=>{
+const drawData = (allUsers) =>{
+    dataWrap.innerHTML=""
+        allUsers.forEach((user,i)=>{
         let tr = createMyElement(dataWrap, "tr") 
         createMyElement(tr, "td", null, i+1)
         createMyElement(tr, "td", null, user.id)
@@ -87,14 +86,88 @@ if(dataWrap){
         createMyElement(td, "a", "btn btn-warning me-3", "edit", "edit.html?id="+user.id)
         let delBtn = createMyElement(td, "button" , "btn btn-danger", "delete")
         delBtn.addEventListener("click", function(e){
-            alert(user.id)
+            delUser(user.id, allUsers)
         })
     })
+
+}
+const dataWrap = document.querySelector("#data-wrap")
+if(dataWrap){
+    const allUsers = readFromLocalStorage()
+    drawData(allUsers)
 }
 // get single user
+const singleUser = document.querySelector("#single-user")
+if(singleUser){
+    // console.log(window.location.search)
+    // console.log(new URLSearchParams(window.location.search))
+    const p = new URLSearchParams(window.location.search)
+    // console.log(p.get("id"));
+    const id = p.get("id")
+    const allUsers = readFromLocalStorage()
+    const data = allUsers.find(user=> user.id == id)
+    console.log(data);
+    if(data){
+        singleUser.innerHTML=`<div class="col-md-12 mb-3">
+                <h3 class="text-center">User <span class="text-primary">${data.name}</span> Data</h3>
+            </div>
+            <div class="col-md-6 col-12 border-start border-5 border-danger mb-3">
+                <h3>User ID: ${data.id}</h3>
+            </div>
+            <div class="col-md-6 col-12 border-start border-5 border-danger mb-3">
+                <h3>User Name: ${data.name}</h3>
+            </div>
+            <div class="col-md-6 col-12 border-start border-5 border-danger mb-3">
+                <h3>User Age: ${data.age}</h3>
+            </div>
+            <div class="col-md-6 col-12 border-start border-5 border-danger mb-3">
+                <h3>User Email: ${data.email}</h3>
+            </div>`
 
+    }
+    else{
+        singleUser.innerHTML = `
+        <div class='alert alert-danger'>user not found</div>
+        <a href="index.html" class="btn btn-danger">back to home</a>
+        `
+    }
+    
+}
 
 // edit user
-
-
+const editUser = document.querySelector("#edit-user")
+if(editUser){
+     const p = new URLSearchParams(window.location.search)
+    const id = p.get("id")
+    const allUsers = readFromLocalStorage()
+    const data = allUsers.find(user=> user.id == id)
+    editUser.innerHTML=`<div class="mb-3">
+                <input class="form-control" name="name" placeholder="enter user name" value="${data.name}">
+            </div>
+            <div class="mb-3">
+                <input class="form-control" name="age" placeholder="enter user age" value="${data.age}">
+            </div>
+            <div class="mb-3">
+                <input class="form-control" name="email" placeholder="enter user email" value="${data.email}">
+            </div>
+            <div class="mb-3 text-end">
+                <button type="submit" class="btn btn-primary">Edit User</button>
+            </div>`
+    editUser.addEventListener("submit", function(e){
+        e.preventDefault()
+        data.name = editUser.name.value
+        data.age = editUser.age.value
+        data.email = editUser.email.value
+        writeToStorage(allUsers)
+        window.location.href = "index.html"
+    })
+}
 // delete user
+
+const delUser = (userId, allUsers) =>{
+    console.log(userId)
+    const data = allUsers.filter(user=> user.id != userId) 
+    console.log(data);
+    writeToStorage(data)
+      drawData(data)
+}
